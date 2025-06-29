1554d3c17d579325125d585b43951e6e3e7c1435
Chirag-1729 <chiragsingh1729@gmail.com>
2025-06-29 18:26:15 +0530

    Updated index.js for Render deployment

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,8 +1,22 @@
-const io = require('socket.io')(8000, {
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
     }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('ChatVerse Server is running ✅');
 });
 
 const users = {};
@@ -15,11 +29,19 @@ io.on('connection', socket => {
     });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id]
+        });
     });
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
 });
