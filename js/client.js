6523f5483fce643e7f0e0a4bac268cca44ad96f8
Chirag-1729 <chiragsingh1729@gmail.com>
2025-06-29 18:28:45 +0530

    Updated client.js with deployed backend URL

--- js/client.js
+++ js/client.js
@@ -1,4 +1,5 @@
-const socket = io('http://localhost:8000');
+// const socket = io('http://localhost:8000');
+const socket = io('https://your-backend-url.onrender.com');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
